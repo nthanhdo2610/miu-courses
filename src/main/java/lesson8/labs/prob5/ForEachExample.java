d566598885b04eef8b91fdf7b60f75f04eb2b28f
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-03-15 08:51:04 -0500

    labs 8

---
+++ src/main/java/lesson8/labs/prob5/ForEachExample.java
@@ -0,0 +1,35 @@
+package lesson8.labs.prob5;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class ForEachExample {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", 
+				"Away", "On Vacation", "Everywhere you want to be");
+		
+		//print each element of the list in upper case format
+		list.forEach(MyClass.INSTANCE);
+		
+	}
+	
+	public static String toUpper(String s) {
+		return s.toUpperCase();
+	}
+	
+	//implement a Consumer
+	enum MyClass implements Consumer<String>{
+
+		INSTANCE;
+
+		@Override
+		public void accept(String s) {
+			System.out.println(toUpper(s));
+		}
+
+	}
+	
+}
