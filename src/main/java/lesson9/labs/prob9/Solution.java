4698c027644bf01f7541c9adb31f27c84052c65e
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-03-18 13:22:27 -0500

    labs 9

--- src/main/java/lesson9/labs/prob9/Solution.java
+++ src/main/java/lesson9/labs/prob9/Solution.java
@@ -1,4 +1,26 @@
 package lesson9.labs.prob9;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Solution {
+    public static void main(String[] args) {
+        printSquares(5);
+    }
+
+    public static void printSquares(int num) {
+        // if num = 4, then your method would output 1, 4, 9, 16
+        System.out.println(
+                Stream.iterate(1,
+                                i -> Math.sqrt(i) <= num,
+                                i -> {
+                                    int n = i + 1;
+                                    while (Math.sqrt(n) % 1 != 0) {
+                                        n++;
+                                    }
+                                    return n;
+                                })
+                        .collect(Collectors.toList())
+        );
+    }
 }
