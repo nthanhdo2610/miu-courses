58265a8393e90f0d82a088bb93db9606d41b71b2
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-03-18 15:24:46 -0500

    labs 10

--- src/main/java/lesson9/labs/prob10a/Or.java
+++ src/main/java/lesson9/labs/prob10a/Or.java
@@ -6,15 +6,18 @@ public class Or {
 
 	public static void main(String[] args) {
 		List<Simple> list = Arrays.asList(new Simple(false), new Simple(false), new Simple(true));
-
+		Or or = new Or();
+		System.out.println(or.someSimpleIsTrue(list));
 	}
-	
+
 	public boolean someSimpleIsTrue(List<Simple> list) {
-		boolean accum = false;
-		for(Simple s: list) {
-			accum = accum || s.flag;
-		}
-		return accum;
+		return list.stream()
+				.reduce(new Simple(false), (s1, s2) -> s2.flag ? s2 : s1).flag;
+//		boolean accum = false;
+//		for(Simple s: list) {
+//			accum = accum || s.flag;
+//		}
+//		return accum;
 	}
 
 }
---
+++ src/main/java/lesson9/labs/prob10b/Solution.java
@@ -0,0 +1,12 @@
+package lesson9.labs.prob10b;
+
+import java.util.stream.Stream;
+
+public class Solution {
+    public static void main(String[] args) {
+        Stream<String> stringStream = Stream.of("Bill", "Thomas", "Mary");
+        System.out.println(
+                stringStream.reduce((s1,s2)-> s1+", " + s2).orElse("")
+        );
+    }
+}
