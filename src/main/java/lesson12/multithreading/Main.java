5ea6386234a7161bd40ad8c18686184646e3e698
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-03-21 10:09:53 -0500

    lesson 12

--- src/main/java/lesson12/multithreading/Main.java
+++ src/main/java/lesson12/multithreading/Main.java
@@ -3,6 +3,23 @@ package lesson12.multithreading;
 public class Main {
 
     public static void main(String[] args) {
-        
+//        System.out.println("main thread");
+//        Thread main = Thread.currentThread();
+//        System.out.println(main.getPriority());
+//        main.setPriority(6);
+//        System.out.println(main.getPriority());
+//        System.out.println(main.getName());
+//        main.setName("Main Thread");
+//        System.out.println(main.getName());
+//        System.out.println("=========");
+//        System.out.println(main);
+
+        // create the first thread
+        MyThread t1 = new MyThread(); // the new thread was born
+        // change thr born state to Ready to Run
+        t1.start();
+        MyThread t2 = new MyThread();
+        t2.start();
+
     }
 }
---
+++ src/main/java/lesson12/multithreading/MyThread.java
@@ -0,0 +1,10 @@
+package lesson12.multithreading;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        for(int i=0; i<5; i++){
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+        }
+    }
+}
